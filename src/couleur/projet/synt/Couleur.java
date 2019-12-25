162b7ae0def23087d843e9c218be8125fe764889
enesa <enesa@192.168.1.11>
2019-12-25 02:18:59 +0100

    Start formes

---
+++ src/couleur/projet/synt/Couleur.java
@@ -0,0 +1,12 @@
+package couleur.projet.synt;
+
+public class Couleur {
+
+	String Couleur;
+
+	public Couleur(String couleur) {
+		this.Couleur = couleur;
+	}
+	
+	
+}
---
+++ src/forme/projet/synt/FormeSimple.java
@@ -0,0 +1,12 @@
+package forme.projet.synt;
+
+public class FormeSimple extends Forme{
+
+
+	@Override
+	void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
