b0fd3aa346f1afe334cf74fa017f9079cf0728de
enesa <enesa@192.168.1.11>
2019-12-25 22:52:17 +0100

    UIDessin - Fini

--- src/serveur/connexion/UIDessin.java
+++ src/serveur/connexion/UIDessin.java
@@ -1,13 +1,53 @@
 package serveur.connexion;
 
+import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Toolkit;
 
 public class UIDessin extends Frame{
-
-	public UIDessin(String client, int x, int y, int l, int h) {
+	public Graphics graphics;
+	
+	/**
+	 * 
+	 * @param client
+	 * @param x
+	 * @param y
+	 * @param l
+	 * @param h
+	 * @throws InterruptedException 
+	 */
+	public UIDessin(String client, int x, int y, int largeur, int hauteur) {
 		super(client);
+
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		Dimension dim = tk.getScreenSize();
+		int le = (int) dim.getWidth();
+		int he = (int) dim.getHeight();
 		
+		int bordGauche, bordSuperieur, l, h;
+		bordGauche = Math.max(0, x);
+		bordSuperieur = Math.max(0, y);
+		l = Math.min(largeur, le - bordGauche);
+		h = Math.min(hauteur, he - bordSuperieur);
 		
+		this.setBounds(bordGauche, bordSuperieur, l, h);
+
+		this.setVisible(true);
+	
+		//=============Active rendering=============//
+		this.setIgnoreRepaint(true);
+		
+		int numBuffers = 1;
+		this.createBufferStrategy(numBuffers);
+		
+		try {
+			Thread.sleep(150);
+		} catch (InterruptedException e) {
+			System.out.println("Erreur Sleep :"+e);
+		}
+		this.graphics = this.getBufferStrategy().getDrawGraphics();
+	
 	}
 	
 	
