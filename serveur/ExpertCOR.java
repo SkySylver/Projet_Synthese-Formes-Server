303c21d6be9ef105273be3928e4ffb44ec259af7
emincan <emin_can@live.fr>
2019-12-25 14:20:36 +0000

    Upload New File

---
+++ serveur/ExpertCOR.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+
+public abstract class ExpertCOR implements Expert {
+    private ExpertCOR suivant;
+    protected abstract boolean dessinerForme(String requete, Graphics2D graphics2D);
+
+    public ExpertCOR() {
+		suivant = null;
+	}
+    
+    public ExpertCOR(ExpertCOR suivant) {
+        this.suivant = suivant;
+    }
+
+    public boolean dessiner(String requete, Graphics2D graphics2D) {
+        if (!this.dessinerForme(requete, graphics2D)) {
+        	if (this.suivant != null)
+        		return this.suivant.dessiner(requete, graphics2D);
+        	else return false;
+        }
+        return true;        
+    }
+}
